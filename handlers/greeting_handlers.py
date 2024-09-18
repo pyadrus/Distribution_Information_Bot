539f506963f2ef21a3c95e5044ba3d51b477c359
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-09-18 16:37:51 +0300

    Миграция с aiogram 2 на aiogram 3

--- handlers/greeting_handlers.py
+++ handlers/greeting_handlers.py
@@ -1,15 +1,15 @@
-from aiogram import types
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 from keyboards.greeting_keyboards import greeting_keyboards
 from messages.greeting_post_massages import greeting_post
 from system.dispatcher import dp
 
 
-@dp.message_handler(commands="start")
-async def start(message: types.Message):
+@dp.message(CommandStart())
+async def start(message: Message):
     """Обработчик команды /start"""
-    keyboards_greeting = greeting_keyboards()
-    await message.answer(greeting_post, reply_markup=keyboards_greeting)
+    await message.answer(greeting_post, reply_markup=greeting_keyboards(), parse_mode="HTML")
 
 
 def start_handler():
--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import asyncio
 import logging
 import sys
 from loguru import logger
-from handlers.check_connected_accounts_handlers import register_check_connected_accounts_handlers
-from handlers.user_account_handlers import account_connection_handler
-from handlers.username_handlers import register_handlers_find_out_username
+# from handlers.check_connected_accounts_handlers import register_check_connected_accounts_handlers
+# from handlers.user_account_handlers import account_connection_handler
+# from handlers.username_handlers import register_handlers_find_out_username
 from handlers.greeting_handlers import start_handler
-from handlers.post_parsing_handlers import register_handlers_post_parsing
-from handlers.parsing_settings_handlers import register_handlers_post_parsing_setting
+# from handlers.post_parsing_handlers import register_handlers_post_parsing
+# from handlers.parsing_settings_handlers import register_handlers_post_parsing_setting
 from system.dispatcher import dp, bot
-from handlers.help_handlers import register_handlers_help
+# from handlers.help_handlers import register_handlers_help
 
 logger.add("logs/log.log", retention="1 days", enqueue=True)  # Логирование бота
 
 async def main():
     await dp.start_polling(bot)
     start_handler()  # Запуск бота через /start
-    account_connection_handler()  # Подключение аккаунта
-    register_handlers_post_parsing_setting()  # Настройка parsing
-    register_handlers_post_parsing()
-    register_handlers_find_out_username()  # Узнать ID группы / канала
-    register_handlers_help()
-    register_check_connected_accounts_handlers()  # Узнать подключенные аккаунты
+    # account_connection_handler()  # Подключение аккаунта
+    # register_handlers_post_parsing_setting()  # Настройка parsing
+    # register_handlers_post_parsing()
+    # register_handlers_find_out_username()  # Узнать ID группы / канала
+    # register_handlers_help()
+    # register_check_connected_accounts_handlers()  # Узнать подключенные аккаунты
 
 
 if __name__ == '__main__':
